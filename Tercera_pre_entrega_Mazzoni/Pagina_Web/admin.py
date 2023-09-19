9ee397b2387b6aea4e033f822ed1e0fa7e3476d9
gianlucamazzoni <142820016+gianlucamazzoni@users.noreply.github.com>
2023-09-19 19:06:28 -0300

    Add files via upload

---
+++ Tercera_pre_entrega_Mazzoni/Pagina_Web/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Lista_Jazz, Lista_Pop, Lista_Rock, Lista_Tango
+# Register your models here.
+admin.site.register(Lista_Jazz)
+
+
+admin.site.register(Lista_Pop)
+
+
+admin.site.register(Lista_Rock)
+
+
+admin.site.register(Lista_Tango)
---
+++ Tercera_pre_entrega_Mazzoni/README.md
@@ -0,0 +1,4 @@
+#Tercera_pre_entrega_Mazzoni
+
+tercera pre-entrega Pagina_Web
+Trata de simular un sistema para almacenar bandas musicales y en que decadas empezaron
